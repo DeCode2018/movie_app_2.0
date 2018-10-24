b08096dc22921bd50eff960b6a3931d03174e1ce
DeCode2018 <decode2018@gmail.com>
2018-10-24 10:05:01 -0400

    removed name column from Users table, added sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_194143) do
+ActiveRecord::Schema.define(version: 2018_10_24_140144) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2018_10_23_194143) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "username"
     t.string "password"
     t.integer "points"
