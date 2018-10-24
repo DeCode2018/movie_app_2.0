b8398d712e8d74ceafd172e82b6063aa7b896f07
suttonau <sutton_mjbc@icloud.com>
2018-10-24 16:27:19 -0400

    Merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,27 +4,27 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def new
     @user = User.new
   end
 
   def show
-  end 
+  end
 
   def edit
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.valid?
-      @user.save
-      session[:user_id] = @user.id
-      redirect_to users_path
-    else
-      render :new
-    end
-  end
+     @user = User.new(user_params)
+     if @user.valid?
+       @user.save
+       session[:user_id] = @user.id
+       redirect_to users_path
+     else
+       render :new
+     end
+   end
 
   def update
     if @user.update(user_params)
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username,:password, :points)
+    params.require(:user).permit(:name,:username,:password, :points)
   end
 
   def find_user
